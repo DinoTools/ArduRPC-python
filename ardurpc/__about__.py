b95c17cdbe3b3caa926bf86659eebf3a7e22d6f3
PhiBo <phibo@dinotools.org>
2014-08-02 22:53:32 +0200

    src/core - Add about file

---
+++ ardurpc/__about__.py
@@ -0,0 +1,22 @@
+__all__ = [
+    "__author__",
+    "__copyright__",
+    "__email__",
+    "__license__",
+    "__summary__",
+    "__title__",
+    "__uri__",
+    "__version__",
+]
+
+__title__ = "ardurpc"
+__summary__ = "An extensible library to control microcontroller boards like Arduino using Python and the ArduRPC protocol"
+__uri__ = "https://github.com/DinoTools/ArduRPC-python"
+
+__version__ = "0.2"
+
+__author__ = "PhiBo (DinoTools)"
+__email__ = ""
+
+__license__ = "LGPLv3+"
+__copyright__ = "Copyright 2014 %s" % __author__
--- ardurpc/__init__.py
+++ ardurpc/__init__.py
@@ -2,8 +2,11 @@ import os
 
 from ardurpc.handler import Handler
 
+from ardurpc.__about__ import (
+    __author__, __copyright__, __email__, __license__, __summary__, __title__,
+    __uri__, __version__
+)
 
-__version__ = "0.2"
 
 _global_handlers = {}
 
